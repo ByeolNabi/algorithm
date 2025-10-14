b6295224394827772ad1f3b195dcf6d485aa314f
Kim Daekyu <82073780+ByeolNabi@users.noreply.github.com>
2025-10-14 22:04:22 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 640 ms, Memory: 50352 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,63 +1,77 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+	static StringTokenizer st;
+	static int N, M;
+	static int cnt;
+	// 동 서 남 북
+	static int[] dy = new int[] { 0, 0, 1, -1 };
+	static int[] dx = new int[] { 1, -1, 0, 0 };
 
-	static int[] dr = {0, 1, 0, -1};
-	static int[] dc = {1, 0, -1, 0};
-	
-	static class Node{
-		int r;
-		int c;
-		public Node(int r, int c) {
-			this.r=r;
-			this.c=c;
-		}
-	}
-	
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		char[][] arr = new char[n][m];
-		
-		Node start = null;
-		
-		for(int i = 0;i<n;i++) {
-			String str = br.readLine();
-			for(int j = 0;j<m;j++) {
-				arr[i][j]=str.charAt(j);
-				if(arr[i][j]=='I')
-					start = new Node(i, j);
-			}
-		}
-		
-		int answer = 0;
-		
-		Queue<Node> q = new ArrayDeque<>();
-		
-		q.offer(start);
-		
-		while(!q.isEmpty()) {
-			Node cur = q.poll();
-			for(int i = 0; i<4;i++) {
-				int r = cur.r+dr[i];
-				int c = cur.c+dc[i];
-				if(r>=0&&r<n&&c>=0&&c<m&&arr[r][c]!='X') {
-					if(arr[r][c]=='P')
-						++answer;
-					arr[r][c]='X';
-					q.offer(new Node(r, c));
+
+		st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N][M];
+		int[][] visited = new int[N][M];
+		int[] start = new int[2];
+
+		for (int i = 0; i < N; i++) {
+			String tmp = br.readLine();
+			for (int j = 0; j < M; j++) {
+				char c = tmp.charAt(j);
+				if (c == 'O') {
+					map[i][j] = 0;
+				} else if (c == 'X') {
+					map[i][j] = 1;
+				} else if (c == 'I') {
+					map[i][j] = 2;
+					start[0] = i;
+					start[1] = j;
+				} else if (c == 'P') {
+					map[i][j] = 3;
 				}
 			}
 		}
-		if(answer==0) System.out.println("TT");
-		else System.out.println(answer);
+
+		cnt = 0;
+		dfs(start[0], start[1], map, visited);
+
+//		for (int i = 0; i < N; i++) {
+//			System.out.println(Arrays.toString(visited[i]));
+//		}
+
+		System.out.println(cnt == 0 ? "TT" : cnt);
 	}
 
+	public static void dfs(int r, int c, int[][] map, int[][] visited) {
+		visited[r][c] = 1;
+		if (map[r][c] == 3) {
+			cnt++;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nxtR, nxtC;
+			nxtR = r + dy[i];
+			nxtC = c + dx[i];
+			if (0 <= nxtR && nxtR < N && 0 <= nxtC && nxtC < M) {
+				if (visited[nxtR][nxtC] == 0) {
+					if (map[nxtR][nxtC] != 1) {
+						dfs(nxtR, nxtC, map, visited);
+					}
+				}
+			}
+		}
+	}
 }
+
+// O : 0 (빈방)
+// X : 1 (벽)
+// I : 2 (도연)
+// P : 3 (사람)
