43bdc8a44cdacee75c62a40d5dc1f1bfc4647799
Kim Daekyu <82073780+ByeolNabi@users.noreply.github.com>
2025-11-06 23:23:07 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2608 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,32 @@
+//수 정렬하기 [브론즈1], 많은 인풋
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int arr[10001];
+int main(){
+    int tc;
+    scanf("%d", &tc);
+    while(tc--){
+        int number;
+        scanf("%d", &number);
+        arr[number]++;
+    }
+    for(int i = 1; i < 10001; ++i){
+        if(arr[i]==0){
+            continue;
+        }
+        else{
+            for(int j = 0; j < arr[i]; ++j){
+               printf("%d\n",i);
+            }
+        }
+    }
+    
+    return 0;
+}
+
+/*
+메모리 초과 : 천만개를 다 저장해서 메모리 초과가 났다.
+해싱을 이용해서 저장하자
+*/
