f9485c3a3bb423e6c05916f557ac7a15a35ef5ce
Kim Daekyu <82073780+ByeolNabi@users.noreply.github.com>
2026-04-05 01:14:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        boolean[] visited = new boolean[10];
+        
+        for(int i = 0; i < numbers.length; i++){
+            visited[numbers[i]] = true;
+        }
+        
+        for(int i = 0; i < 10; i++){
+            if(visited[i] == false){
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
