a13322440127f6ef2cb1eba32a05710403819e3d
Kim Daekyu <82073780+ByeolNabi@users.noreply.github.com>
2026-05-07 22:57:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.08 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -5,21 +5,19 @@ using namespace std;
 
 string solution(string s) {
     int flag = true;
-    
     for(int i = 0; i < s.length(); i++){
         if(s[i] == ' '){
             flag = true;
         }else{
             if(flag){
                 s[i] = toupper(s[i]);
-                flag = false;
+                 flag=false;
             }else{
                 s[i] = tolower(s[i]);
+               
             }
         }
-    }
+    }   
+    
     return s;
 }
-
-// 일단 전체를 돌아보면서 flag가 활성화되어있으면서 영어가 나왔으면 높이기
-// 
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 3.75 MB, 시간: 0.02 ms
+메모리: 4.08 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 07일 22:43:11
+2026년 05월 07일 22:57:14
 
 ### 문제 설명
 
